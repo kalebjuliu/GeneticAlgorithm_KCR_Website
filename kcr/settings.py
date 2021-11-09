32728ff343b83b4716fe203f769917c8b7eae5ca
Kaleb Juliu <kalebjuliu@gmail.com>
2021-11-09 22:34:08 +0700

    Update settings.py

--- kcr/settings.py
+++ kcr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3-qu--p92nwi!=in-aqplq_ju6vqsd6s5bovc=0(94r7o95%kw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
