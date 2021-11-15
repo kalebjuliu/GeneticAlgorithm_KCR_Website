234c5b56e095c23a6fb2250fb3673703bb24a00b
Kaleb Juliu <kalebjuliu@gmail.com>
2021-11-15 19:01:03 +0700

    Update urls

--- kcr/urls.py
+++ kcr/urls.py
@@ -23,8 +23,8 @@ from django.conf.urls import url
 urlpatterns = [
     path('', include('diet.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,
+    #     {'document_root':       settings.MEDIA_ROOT}),
+    # url(r'^static/(?P<path>.*)$', serve,
+    #     {'document_root': settings.STATIC_ROOT}),
 ]
